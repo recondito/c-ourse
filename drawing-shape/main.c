6b427f881c52d0c7065775a1c2cfbb7671bce767
recondito <santanallanes@hotmail.com>
2024-12-17 20:02:16 -0400

    drawing-shape completed.

---
+++ drawing-shape/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+    printf("        /l\n");
+    printf("      /  l\n");
+    printf("    /    l\n");
+    printf("  /      l\n");
+    printf("/____ l\n");
+
+    return 0;
+}
