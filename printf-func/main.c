5656182f56c4004626ef8b55e55df7e8ca2d0ba1
recondito <santanallanes@hotmail.com>
2024-12-17 20:34:38 -0400

    printf-func completed.

---
+++ printf-func/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+    printf("Hello, world!\n"); // New line operator.
+    printf("Hello,\"world!\n"); // Escape character.
+    printf("My favorite %s is %d\n", "number", 5); // Format specifier (string and number).
+    return 0;
+}
