d60f712b773b03a1b18b772f2b8c87bc3857d979
recondito <santanallanes@hotmail.com>
2024-12-17 20:55:50 -0400

    Added Power function.

--- numeric-data/main.c
+++ numeric-data/main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main () {
     printf("%f\n", 5.0 + 4.5); // Same type
     printf("%f\n", 5 + 4.5); // Gets converted to floating point number
     printf("%d\n", 5 + 4); //  Returns an integer
     printf("%d\n", 5 / 4); // Gets converted to integer even if the result is not exact.
+    printf("%f\n", pow(2, 3)); // 2 to the power of 3.
 
     return 0;
 }
