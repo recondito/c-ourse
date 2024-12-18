d5854f9a0f46c45249806b5a33e39d448ea30031
recondito <santanallanes@hotmail.com>
2024-12-17 20:28:42 -0400

    data-types completed.

---
+++ data-types/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+    int age = 24;
+    const float piGrams = 3.1415926;
+    double gpa = 3.58;
+    const char grades[] = { 'A', 'B', 'C', 'D', 'F' };
+
+    return 0;
+}
