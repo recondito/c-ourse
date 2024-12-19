b7d41433129056b7929a0f594761bf000d20c067
recondito <santanallanes@hotmail.com>
2024-12-19 18:53:44 -0400

    Completed user-input.

--- constants/main.c
+++ constants/main.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 int main () {
     
     const int number = 5;
     printf("%d\n", number);
-    number = 8;
+    // number = 8;
     printf("%d\n", number);
 
     // Will not compile as constants cannot be modified.
