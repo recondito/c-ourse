fceb391b151bdbc209feb3070ca006f7d18b0002
recondito <santanallanes@hotmail.com>
2024-12-19 19:11:36 -0400

    Corrections.

--- user-input/main.c
+++ user-input/main.c
@@ -6,6 +6,12 @@ int main () {
     printf("Enter your age: ");
     scanf("%d", &age);
     printf("You are %d years old.\n", age);
+    getchar();
+
+    char name[20];  
+    printf("Enter your name: ");
+    fgets(name, 20, stdin); // No '&' needed as the name of an array is the same as &name[0]
+    printf("Your name is %s", name);
 
     return 0;
 } 
