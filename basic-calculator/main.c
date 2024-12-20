b0a7181016ceca55064e627618fc22b18976b74d
recondito <santanallanes@hotmail.com>
2024-12-19 20:07:45 -0400

    basic-calculator completed

---
+++ basic-calculator/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+    int x;
+    int y;
+    printf("Enter first number: ");
+    scanf("%d", &x);
+    printf("Enter second number: ");
+    scanf("%d", &y);
+
+    printf("Answer: %d\n", x + y);
+
+    return 0;
+} 
